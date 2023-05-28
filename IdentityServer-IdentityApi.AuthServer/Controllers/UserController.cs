80ed35e70e0d5494d2ff29cd56ae2f64c8794af2
javanshirli_o@itstep.academy <javanshirli_o@itstep.academy>
2023-05-28 16:59:37 -0700

    I Write code by SignUp Function

--- IdentityServer-IdentityApi.AuthServer/Controllers/UserController.cs
+++ IdentityServer-IdentityApi.AuthServer/Controllers/UserController.cs
@@ -1,19 +1,42 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityServer.IdentityApi.AuthServer.Models;
+using IdentityServer_IdentityApi.AuthServer.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 using static IdentityServer4.IdentityServerConstants;
 
 namespace IdentityServer.IdentityApi.AuthServer.Controllers
 {
-  
+
     [Route("api/[controller]/[action]")]
     [Authorize(LocalApi.PolicyName)]
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult SignUp()
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
         {
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserSaveViewModel request)
+        {
+            ApplicationUser applicationUser = new ApplicationUser();
+
+            applicationUser.UserName = request.Username;
+            applicationUser.Email = request.Email;
+            applicationUser.City = request.City;
+
+            var result = await this.userManager.CreateAsync(applicationUser, request.Password);
+
+            if(!result.Succeeded)
+                return BadRequest(result.Errors.Select(x=>x.Description));
+
             return Ok("signup calisti");
         }
     }
